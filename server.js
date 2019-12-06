d52b2e8b5a2f235e92ca9fd598c9333d1cebc4a9
bwilm <32461222+bwilm@users.noreply.github.com>
2019-12-05 23:01:41 -0500

    server change 3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/BaliOne/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 console.log('listenin on port 8080')
